almost there. There is something wrong with the Neumann BCs I believe. ALso need to code the zero gradient for the network. Next thing Ima redo the one vessel simulation to make sure it is operable, to facilitate the final work over the reconstruction, and to ensure the Neumann BCs work well.

Synthetic_Network_2/Full_Synth_Network.py
@@ -19,14 +19,14 @@ from numba import njit
 import scipy as sp
 #%
 
-
+import matplotlib.pyplot as plt
 #%
 import os
 os.chdir('/home/pdavid/Bureau/Code/hybrid3d/src_final')
 from mesh_1D import mesh_1D
 from hybridFast import hybrid_set_up
 from mesh import cart_mesh_3D
-
+from post_processing import Visualization3D
 from assembly_1D import AssembleVertexToEdge, PreProcessingNetwork, CheckLocalConservativenessFlowRate, CheckLocalConservativenessVelocity
 
 def SplitFile(filename, output_dir_network):
@@ -81,7 +81,7 @@ def SetArtificialBCs(vertex_to_edge, entry_concentration, exit_concentration, in
 
 #%%
 
-cells_3D=10
+cells_3D=20
 output_dir_network=os.path.join(path_network ,"Files/divided_files")
 
 #output_dir_network = '/home/pdavid/Bureau/PhD/Network_Flo/All_files/Split/'  # Specify the output directory here
@@ -137,12 +137,12 @@ df = pd.read_csv(output_dir_network + '/output_6.txt', skiprows=1, sep="\s+", na
 with open(output_dir_network + '/output_6.txt', 'r') as file:
     # Read the first line
     output_six= file.readline()
-Flow_rate=np.ndarray.flatten(df.values)
+Flow_rate=np.ndarray.flatten(df.values)*1e3
 
 #%%
-K=np.average(diameters)/np.ndarray.flatten(diameters)
+K=np.average(diameters)/np.ndarray.flatten(diameters)/4
 #The flow rate is given in nl/s
-U = 4*Flow_rate/np.pi/diameters**2*1e6  #To convert to speed in micrometer/second
+U = 4*Flow_rate/np.pi/diameters**2*1e9 #To convert to speed in micrometer/second
 
 startVertex=edges[:,0].copy()
 endVertex=edges[:,1].copy()
@@ -184,7 +184,9 @@ L_3D=np.array([305,305,305])
 #Set artificial BCs for the network 
 BCs_1D=SetArtificialBCs(vertex_to_edge, 1,0, startVertex, endVertex)
 
-BC_type=np.array(["Dirichlet", "Dirichlet", "Neumann","Neumann","Neumann","Neumann"])
+#BC_type=np.array(["Dirichlet", "Neumann","Neumann","Neumann","Neumann","Neumann"])
+#BC_type=np.array(["Dirichlet","Dirichlet","Dirichlet","Dirichlet","Dirichlet","Dirichlet"])
+BC_type=np.array(["Neumann", "Neumann", "Neumann","Neumann","Neumann","Neumann"])
 BC_value=np.array([0,0,0,0,0,0])
 
 net=mesh_1D( startVertex, endVertex, vertex_to_edge ,pos_vertex, diameters, np.average(diameters)/2,np.average(U))
@@ -194,14 +196,321 @@ mesh=cart_mesh_3D(L_3D,cells_3D)
 net.PositionalArraysFast(mesh)
 #%%
 
-mat_path="/home/pdavid/Bureau/Code/hybrid3d/Synthetic_Network_2"
+mat_path="/home/pdavid/Bureau/Code/hybrid3d/Synthetic_Network_2/F20"
 phi_bar_path=os.path.join(mat_path, "matrix_phi_bar")
 
-n=1
+n=2
 prob=hybrid_set_up(mesh, net, BC_type, BC_value,n,1, K, BCs_1D)
-#prob.phi_bar_bool=True
-#prob.B_assembly_bool=True
-prob.AssemblyProblem(mat_path,phi_bar_path)
+prob.phi_bar_bool=True 
+prob.B_assembly_bool=True
+prob.I_assembly_bool=True
+#%%
+M_D=0.01
+prob.AssemblyProblem(mat_path)
+prob.Full_ind_array[:cells_3D**2]-=M_D*mesh.h**2
+
+#%% - Constant concentration
+constant_concentration=0
+if constant_concentration:
+    from scipy.sparse.linalg import spsolve as dir_solve
+    Up=sp.sparse.hstack((prob.A_matrix, prob.B_matrix))
+    Down=sp.sparse.hstack((prob.D_matrix, prob.Gij + prob.q_portion))
+    
+    prob.Cv=np.ones(prob.S)
+    
+    ind_array=np.concatenate((prob.I_ind_array, prob.F_matrix.dot(prob.Cv)))
+    
+    L=sp.sparse.vstack((Up, Down))
+    
+    sol=dir_solve(L,-ind_array)
+    
+    prob.q=sol[-prob.S:]
+    prob.s=sol[:-prob.S]
 
 #%%
+# =============================================================================
+# if not prob.B_assembly_bool and os.path.exists(mat_path + "/q.npy"): 
+#     np.load( mat_path + "/q.npy",prob.q)
+#     np.load( mat_path + "/s.npy",prob.s)
+#     np.load( mat_path + "/Cv.npy",prob.Cv)
+# else:
+#     prob.SolveProblem()
+#     np.save( mat_path + "/q",prob.q)
+#     np.save( mat_path + "/s",prob.s)
+#     np.save( mat_path + "/Cv",prob.Cv)
+# =============================================================================
+
 prob.SolveProblem()
+    
+#%%
+
+
+#post=Visualization3D((mesh.h/2,L_3D[0]/2-mesh.h/2), 50, prob, 12, 0.5)
+
+#%%
+
+def get_phi_bar(phi_bar_path, s, q):
+    phi_bar_s=sp.sparse.load_npz( phi_bar_path + "/phi_bar_s.npz")
+    phi_bar_q=sp.sparse.load_npz( phi_bar_path + "/phi_bar_q.npz")
+    
+    phi_bar=phi_bar_s.dot(s)+phi_bar_q.dot(q)
+    return phi_bar
+
+phi_bar=get_phi_bar(mat_path, prob.s, prob.q)
+
+#%%
+from post_processing import *
+points=np.array([1/6,3/6,5/6])*L_3D[0]
+
+ll=100
+crds=np.zeros([9,3,ll])
+
+for i in range(3):
+    for j in range(3):
+        y=np.linspace(0,L_3D[0], ll)*0.88+L_3D[0]*0.05
+        x=np.zeros(ll)+points[i]
+        z=np.zeros(ll)+points[j]
+        
+        crds[j+3*i, 0,:]=y
+        crds[j+3*i, 1,:]=x
+        crds[j+3*i, 2,:]=z
+    
+InterpolateHelper((prob, crds[0,:,20]))
+
+#%%
+import dask
+from dask import delayed
+
+phi = []
+for i in range(9):
+    phi.append(delayed(ReconstructionCoordinatesFast)(crds[i, :, :].T, n, prob.mesh_3D.cells_x, 
+                                                      prob.mesh_3D.cells_y,prob.mesh_3D.cells_z, prob.mesh_3D.h,
+                                                      prob.mesh_3D.pos_cells,prob.mesh_1D.s_blocks, 
+                                                      prob.mesh_1D.source_edge,prob.mesh_1D.tau, 
+                                                      prob.mesh_1D.pos_s, prob.mesh_1D.h, prob.mesh_1D.R, 
+                                                      1, prob.s, prob.q))
+
+phi_array = np.array(dask.compute(*phi))
+plt.plot(phi_array.T)
+plt.xlabel("y")
+plt.show()
+#%% - plane
+# =============================================================================
+# %%time
+# phi = []
+# for i in range(ll):
+#     phi.append(delayed(ReconstructionCoordinatesFast)(np.array([y, np.zeros(ll)+ y[i], np.zeros(ll)+ L_3D[0]/2]).T, prob))
+# 
+# phi_array = dask.compute(*phi)
+# 
+# 
+# #%%
+# 
+# %%time
+# from small_functions import GetBoundaryStatus
+# phi = np.zeros([ll,ll])
+# for i in range(ll):
+#     crds=np.array([y, np.zeros(ll)+ y[i], np.zeros(ll)+ L_3D[0]/4*3]).T
+#     
+#     phi[i]=ReconstructionCoordinatesFast(crds, n, prob.mesh_3D.cells_x, prob.mesh_3D.cells_y,prob.mesh_3D.cells_z,
+#                                          prob.mesh_3D.h,prob.mesh_3D.pos_cells,
+#                                          prob.mesh_1D.s_blocks, prob.mesh_1D.source_edge,prob.mesh_1D.tau, prob.mesh_1D.pos_s, prob.mesh_1D.h, 
+#                                          prob.R, 1, prob.s, prob.q)
+# 
+# plt.imshow(phi, origin="lower")
+# plt.xlabel("x")
+# plt.ylabel("y")
+# 
+# =============================================================================
+
+
+#%%
+from post_processing import *
+def GetPlaneReconstructionFast(plane_coord,plane_axis, i_axis, j_axis,corners, resolution, prob):
+    crds=GetCoordsPlane(corners, resolution)
+    mask=GetPlaneIntravascularComponent(plane_coord, prob.mesh_1D.pos_s, prob.mesh_1D.source_edge, 
+                                        plane_axis, i_axis, j_axis, corners, prob.mesh_1D.tau, 
+                                        resolution, prob.mesh_1D.R, prob.mesh_1D.h, prob.mesh_1D.cells)
+    intra=prob.Cv[mask-1]
+    result = np.where(mask == 0, np.nan, intra)
+    new_mask=mask > 0
+    
+    phi=ReconstructionCoordinatesFast(crds, n, prob.mesh_3D.cells_x, prob.mesh_3D.cells_y,prob.mesh_3D.cells_z,
+                                         prob.mesh_3D.h,prob.mesh_3D.pos_cells,
+                                         prob.mesh_1D.s_blocks, prob.mesh_1D.source_edge,prob.mesh_1D.tau, prob.mesh_1D.pos_s, prob.mesh_1D.h, 
+                                         prob.R, 1, prob.s, prob.q)
+    plt.imshow(mask)
+    plt.show()
+    
+    plt.imshow(phi.reshape(resolution,resolution))
+    plt.show()
+    
+    plt.imshow(result)
+    plt.show()
+    
+    phi_final=phi.reshape(resolution,resolution)
+    phi_final[new_mask]=result[new_mask]
+    
+    return  phi_final,result,mask
+
+def GetPlaneReconstructionParallel(plane_coord,plane_axis, i_axis, j_axis,corners, resolution, prob, property_array):
+    crds=GetCoordsPlane(corners, resolution)
+    mask=GetPlaneIntravascularComponent(plane_coord, prob.mesh_1D.pos_s, prob.mesh_1D.source_edge, 
+                                        plane_axis, i_axis, j_axis, corners, prob.mesh_1D.tau, 
+                                        resolution, prob.mesh_1D.R, prob.mesh_1D.h, prob.mesh_1D.cells)
+    intra=property_array[mask-1]
+    result = np.where(mask == 0, np.nan, intra)
+    new_mask=mask > 0
+    
+    phi=ReconstructionCoordinatesParallel(crds, n, prob.mesh_3D.cells_x, prob.mesh_3D.cells_y,prob.mesh_3D.cells_z,
+                                         prob.mesh_3D.h,prob.mesh_3D.pos_cells,
+                                         prob.mesh_1D.s_blocks, prob.mesh_1D.source_edge,prob.mesh_1D.tau, prob.mesh_1D.pos_s, prob.mesh_1D.h, 
+                                         prob.R, 1, prob.s, prob.q)
+    phi2=phi.copy()
+    
+    plt.imshow(phi.reshape(resolution,resolution), origin="lower")
+    plt.show()
+    
+    plt.imshow(result, origin="lower")
+    plt.show()
+    
+    phi_final=phi.reshape(resolution,resolution)
+    phi_final[new_mask]=result[new_mask]
+    
+    return phi_final,result,phi2.reshape(resolution, resolution)
+        
+        
+def GetPlaneIntravascularComponent(plane_coord, pos_s, source_edge,
+                                   plane_axis, i_axis, j_axis, corners, 
+                                   tau_array, resolution,R, h_1D, cells_per_segment):
+    """This function aims to provide the voxels of the plane defined by plane_coord whose center fall
+    within a source cylinder. We work in 2D so it is not as confusing
+    1st - We figure out which sources intersect the plane 
+    2nd - Out of those possible sources, we loop through each of them to find which voxels fall within the cylinder
+    
+    plane_coord -> The coordinates of the plane on the axis perpendicular to itself
+    pos_s -> position of the sources
+    plane_axis -> 1, 2 or 3 for x, y or z
+    corners_plane -> self explanatory
+    tau_array -> the axial unitary vector for each source
+    i_axis -> axis that we want to be in the horizontal direction of the matrix
+    j_axis -> axis that we want to be in the vertical direction of the matrix
+    
+    Corners are given in the following order:
+        (0,0), (0,1), (1,0), (1,1)
+    where the first entry is the horizontal direction and the second is the vertical direction
+    according to the i_axis and j_axis"""
+    
+    #crds=GetCoordsPlane(corners, resolution)
+    #crds_plane=np.delete(crds, plane_axis, axis=1) #We eliminate the component perpendicular to the plane
+    
+    #First - Figure out the sources that intersect the plane
+    pos_s_plane=pos_s[:,plane_axis]
+    sources=np.where(np.abs(pos_s_plane - plane_coord) < np.repeat(h_1D, cells_per_segment)/2)[0]
+    
+    mask=np.zeros((resolution, resolution), dtype=np.int64)
+    
+    tau=np.linalg.norm((corners[2]-corners[0])/resolution)
+    h=np.linalg.norm((corners[1]-corners[0])/resolution)
+    
+    x=np.linspace(corners[0,i_axis]+tau/2, corners[2,i_axis]-tau/2, resolution)
+    y=np.linspace(corners[0,j_axis]+h/2, corners[1,j_axis]-h/2, resolution)
+    X,Y=np.meshgrid(x,y)
+    
+    for s in sources:
+# =============================================================================
+#         x_dist=X-pos_s[s,i_axis]
+#         y_dist=Y-pos_s[s,j_axis]
+#         
+#         a=(x_dist*tau_array[s,i_axis]**2+y_dist*tau_array[s,j_axis]**2 < h_1D[s]**2)
+#         b=(x_dist*tau_array[s,j_axis]**2+y_dist*tau_array[s,i_axis]**2 < R[s]**2)
+#         d=np.where(a & b)
+# =============================================================================
+        P = np.stack((X - pos_s[s,0], Y - pos_s[s,1], np.zeros_like(X)), axis=-1)
+        
+        # Project the position vectors onto the direction vector to get the scalar value t
+        v=tau_array[source_edge[s]]
+        t = np.sum(P *v, axis=-1)
+        
+        # Calculate the perpendicular distance of each point from the cylinder axis
+        d = np.linalg.norm(P - (t[:, :, np.newaxis] * v), axis=-1)
+        
+        # Find the indices of points that are within the cylinder
+        indices = np.where((d <= R[source_edge[s]]) & (np.abs(t) <= h_1D[source_edge[s]]/2))
+        mask[indices]=s+1
+    return mask
+
+
+def GetCoordsPlane(corners, resolution):
+    """We imagine the plane with a horizontal (x) and vertical direction (y).
+    Corners are given in the following order:
+        (0,0), (0,1), (1,0), (1,1)
+        
+        - tau indicates the discretization size in horizontal direction
+        - h indicates the discretization size in vertical direcion"""
+    crds=np.zeros((0,3))
+    tau=(corners[2]-corners[0])/resolution
+    
+    h=(corners[1]-corners[0])/resolution
+    L_h=np.linalg.norm((corners[1]-corners[0]))
+    
+    local_array= np.linspace(corners[0]+tau/2, corners[2]-tau/2 , resolution ) #along the horizontal direction
+    for j in range(resolution):
+        arr=local_array.copy()
+        arr[:,0]+=h[0]*(j+1/2)
+        arr[:,1]+=h[1]*(j+1/2)
+        arr[:,2]+=h[2]*(j+1/2)
+        
+        crds=np.vstack((crds, arr))
+    return(crds)
+
+corners=np.array([[5,5,150],[5,300,150],[300,5,150],[300,300,150]])
+# =============================================================================
+# hi=GetPlaneReconstructionParallel(150, 2, 0,1,corners , 150, prob)
+# #%%
+# plt.imshow(hi, origin="lower", vmax=0.8)
+# plt.colorbar()
+# plt.show()
+# 
+# =============================================================================
+#%%
+import time
+# =============================================================================
+# begin=time.time()
+# hi=GetPlaneReconstructionParallel(150, 2, 0,1,corners , 500, prob)
+# end=time.time()
+# a=end-begin
+# print(a)
+# 
+# begin=time.time()
+# hi=GetPlaneReconstructionFast(150, 2, 0,1,corners , 500, prob)
+# end=time.time()
+# b=end-begin
+# =============================================================================
+
+#%%
+P_per_edge=Pressure[np.arange(len(Pressure)/2, dtype=int)*2]
+P_per_source=np.repeat(P_per_edge, net.cells)    
+
+phi=[]
+for z in np.array([0.2,0.4,0.6,0.8])*L_3D[0]:
+    corners=np.array([[5,5,z],[5,300,z],[300,5,z],[300,300,z]])
+    begin=time.time()
+    phi.append(GetPlaneReconstructionParallel(z, 2, 0,1,corners , 100, prob, prob.Cv))
+    end=time.time()
+    a=end-begin
+    print(a)
+
+#%%
+for i in range(4):
+    plt.imshow(phi[i][2], origin="lower", vmax=0.8)
+    plt.colorbar()
+    plt.show()
+# =============================================================================
+#     plt.imshow(phi[i][1], origin="lower")
+#     plt.colorbar()
+#     plt.show()
+#     plt.imshow(phi[i][2], origin="lower")
+#     plt.colorbar()
+#     plt.show()
+# =============================================================================

src_final/Second_eq_functions.py
@@ -251,7 +251,7 @@ def InterpolatePhiBarBlock(block,n, cells_x, cells_y, cells_z, h_3D,
     return(kernel_s_array, row_s_array, col_s_array, 
            kernel_q_array, row_q_array,col_q_array)
 
-def RetrievePhiBar(mat_path, S, size_mesh, uni_s_blocks):
+def RetrievePhiBar(mat_path, phi_bar_path, S, size_mesh, uni_s_blocks):
     
     kernel_q=np.zeros(0, dtype=np.float64)
     kernel_s=np.zeros(0, dtype=np.float64)
@@ -271,7 +271,7 @@ def RetrievePhiBar(mat_path, S, size_mesh, uni_s_blocks):
         print("Retrieve block: ", i)
         c+=1
         print(c)
-        list_of_kernels=RetrieveBlockPhiBar(mat_path,i)
+        list_of_kernels=RetrieveBlockPhiBar(phi_bar_path,i)
         
         kernel_s=np.concatenate((kernel_s, list_of_kernels[0]))
         kernel_q=np.concatenate((kernel_q, list_of_kernels[3]))
@@ -299,8 +299,8 @@ def RetrievePhiBar(mat_path, S, size_mesh, uni_s_blocks):
             phi_bar_s+=sp.sparse.csc_matrix((kernel_s,(row_s, col_s)), shape=(S, size_mesh))
             phi_bar_q+=sp.sparse.csc_matrix((kernel_q,(row_q, col_q)), shape=(S, S))
             
-            sp.sparse.save_npz(mat_path + 'phi_bar_s', phi_bar_s)
-            sp.sparse.save_npz(mat_path + 'phi_bar_q', phi_bar_q)
+            sp.sparse.save_npz(mat_path + '/phi_bar_s', phi_bar_s)
+            sp.sparse.save_npz(mat_path + '/phi_bar_q', phi_bar_q)
             
     return phi_bar_s, phi_bar_q
    

src_final/Tests_1vessel/test_1vessel_extravascular.py
@@ -47,10 +47,11 @@ import matplotlib.pyplot as plt
 import math
 
 from mesh_1D import mesh_1D
-from Green import GetSourcePotential
+from GreenFast import GetSourcePotential
 import pdb
 
-from hybrid_set_up_noboundary import hybrid_set_up, Visualization3D
+from hybridFast import hybrid_set_up
+from post_processing import Visualization3D 
 
 from neighbourhood import GetNeighbourhood, GetUncommon
 
@@ -97,7 +98,7 @@ BCs_1D=np.array([[0,1],
 prob=hybrid_set_up(mesh, net, BC_type, BC_value,n,1, np.zeros(len(diameters))+K, BCs_1D)
 
 mesh.GetOrderedConnectivityMatrix()
-prob.AssemblyProblem()
+prob.AssemblyProblem(path + "/matrices")
 
 print("If all BCs are newton the sum of all coefficients divided by the length of the network should be close to 1", np.sum(prob.B_matrix.toarray())/net.L)
 

src_final/hybridFast.py
@@ -81,8 +81,9 @@ class hybrid_set_up():
         
         self.DetermineMaxSize()
         
-        self.B_assembly_bool=0 #True if the matrix has already been computed
-        self.phi_bar_bool=0 #True if the matrix has already been computed
+        self.B_assembly_bool=False #True if the matrix has already been computed
+        self.phi_bar_bool=False #True if the matrix has already been computed
+        self.I_assembly_bool=False
         
         return
     
@@ -109,7 +110,7 @@ class hybrid_set_up():
         else:
             print("loading matrix from: ", mat_path)
             B_matrix=sp.sparse.load_npz(mat_path + "/B_matrix.npz")
-        
+            print("Finished loading matrix")
         B_matrix=self.AssemblyBBoundaries(B_matrix)
         self.B_matrix=B_matrix
         
@@ -227,15 +228,18 @@ class hybrid_set_up():
         
         return sp.sparse.hstack((self.D_matrix_slow, self.E_matrix_slow, self.F_matrix_slow))
     
-    def AssemblyDEFFast(self, path_phi_bar):
+    def AssemblyDEFFast(self, path_phi_bar, mat_path):
         if self.phi_bar_bool:
-            self.phi_bar_s=sp.sparse.load_npz(path_phi_bar + '/phi_bar_s.npz')
-            self.Gij=sp.sparse.load_npz(path_phi_bar + '/phi_bar_q.npz')
-            
+            print("Began laoding phi_bar from ", mat_path)
+            self.phi_bar_s=sp.sparse.load_npz(mat_path + '/phi_bar_s.npz')
+            self.Gij=sp.sparse.load_npz(mat_path + '/phi_bar_q.npz')
+            print("Finished laoding phi_bar from ", mat_path)
         else:
+            if not os.path.exists(path_phi_bar):
+                os.mkdir(path_phi_bar)
             print("Calculating phi_bar")
             self.InterpolatePhiFullFast(path_phi_bar, 1)
-            self.phi_bar_s, self.Gij=RetrievePhiBar(path_phi_bar,self.S, self.mesh_3D.size_mesh, self.mesh_1D.uni_s_blocks)
+            self.phi_bar_s, self.Gij=RetrievePhiBar(mat_path, path_phi_bar,self.S, self.mesh_3D.size_mesh, self.mesh_1D.uni_s_blocks)
         self.D_matrix=self.phi_bar_s
         
         q_portion_diagonal=np.repeat(1/self.K, self.mesh_1D.cells) #q_j/K_j
@@ -244,7 +248,7 @@ class hybrid_set_up():
         self.Gij+=self.q_portion
         self.F_matrix=-sp.sparse.diags(np.ones(len(self.mesh_1D.s_blocks)))
         self.D_E_F_matrix=sp.sparse.hstack((self.Gij, self.F_matrix))
-        self.Gij=None
+        #self.Gij=None
         self.D_E_F_matrix=sp.sparse.hstack((self.D_matrix, self.D_E_F_matrix))
         
         return self.D_E_F_matrix
@@ -259,29 +263,31 @@ class hybrid_set_up():
         dask.compute(a)
         return 
     
-    def RetrievePhiBar(self, path):
-        kernel_q=np.zeros(0, dtype=np.float64)
-        kernel_s=np.zeros(0, dtype=np.float64)
-        #The kernels with the positions
-        col_s=np.zeros(0, dtype=np.int64)
-        col_q=np.zeros(0, dtype=np.int64)
-        
-        row_s=np.zeros(0, dtype=np.int64)
-        row_q=np.zeros(0, dtype=np.int64)
-        
-        for i in self.mesh_1D.uni_s_blocks:
-            kernel_s=np.concatenate((kernel_s, np.load(path + '/{}_kernel_s.npy'.format(i))))
-            kernel_q=np.concatenate((kernel_q, np.load(path + '/{}_kernel_s.npy'.format(i))))
-            col_s=np.concatenate((col_s, np.load(path + '/{}_kernel_s.npy'.format(i))))
-            col_q=np.concatenate((col_q, np.load(path + '/{}_kernel_s.npy'.format(i))))
-            row_s=np.concatenate((row_s, np.load(path + '/{}_kernel_s.npy'.format(i))))
-            row_q=np.concatenate((row_q, np.load(path + '/{}_kernel_s.npy'.format(i))))
-            
-            
-        return kernel_s,row_s, col_s,kernel_q, row_q, col_q
+# =============================================================================
+#     def RetrievePhiBar(self, path):
+#         kernel_q=np.zeros(0, dtype=np.float64)
+#         kernel_s=np.zeros(0, dtype=np.float64)
+#         #The kernels with the positions
+#         col_s=np.zeros(0, dtype=np.int64)
+#         col_q=np.zeros(0, dtype=np.int64)
+#         
+#         row_s=np.zeros(0, dtype=np.int64)
+#         row_q=np.zeros(0, dtype=np.int64)
+#         
+#         for i in self.mesh_1D.uni_s_blocks:
+#             kernel_s=np.concatenate((kernel_s, np.load(path + '/{}_kernel_s.npy'.format(i))))
+#             kernel_q=np.concatenate((kernel_q, np.load(path + '/{}_kernel_s.npy'.format(i))))
+#             col_s=np.concatenate((col_s, np.load(path + '/{}_kernel_s.npy'.format(i))))
+#             col_q=np.concatenate((col_q, np.load(path + '/{}_kernel_s.npy'.format(i))))
+#             row_s=np.concatenate((row_s, np.load(path + '/{}_kernel_s.npy'.format(i))))
+#             row_q=np.concatenate((row_q, np.load(path + '/{}_kernel_s.npy'.format(i))))
+#             
+#             
+#         return kernel_s,row_s, col_s,kernel_q, row_q, col_q
+# =============================================================================
 
-    def AssemblyGHI(self):
-        I_matrix=self.AssemblyI()
+    def AssemblyGHI(self, path_I):
+        I_matrix=self.AssemblyI(path_I)
         
         #WILL CHANGE WHEN CONSIDERING MORE THAN 1 VESSEL
         aux_arr=np.zeros(len(self.mesh_1D.pos_s))
@@ -301,31 +307,36 @@ class hybrid_set_up():
         
         return(self.G_H_I_matrix)
     
-    def AssemblyI(self):
+    def AssemblyI(self, path_I):
         """Models intravascular transport. Advection-diffusion equation
         
         FOR NOW IT ONLY HANDLES A SINGLE VESSEL"""
         
         D=self.mesh_1D.D
         U=self.mesh_1D.U
-        L=self.mesh_1D.L
-        aa, ind_array, DoF=FullAdvectionDiffusion1D(U, D, self.mesh_1D.h, self.mesh_1D.cells, self.mesh_1D.startVertex, self.mesh_1D.vertex_to_edge, self.R, self.BCs_1D)
-# =============================================================================
-#         aa, bb =AssemblyTransport1D(U, D, L[0]/len(self.mesh_1D.pos_s), len(self.mesh_1D.pos_s))
-#         self.III_ind_array = np.zeros(len(self.mesh_1D.pos_s))
-#         self.III_ind_array [0] = bb[0]
-# =============================================================================
-        self.III_ind_array=ind_array
-        I = csc_matrix((aa[0], (aa[1], aa[2])), shape=(len(self.mesh_1D.pos_s), len(self.mesh_1D.pos_s)))
         
+        L=self.mesh_1D.L
+        
+        
+        if not self.I_assembly_bool:
+            aa, ind_array, DoF=FullAdvectionDiffusion1D(U, D, self.mesh_1D.h, self.mesh_1D.cells, self.mesh_1D.startVertex, self.mesh_1D.vertex_to_edge, self.R, self.BCs_1D)
+            self.III_ind_array=ind_array
+            I = csc_matrix((aa[0], (aa[1], aa[2])), shape=(len(self.mesh_1D.pos_s), len(self.mesh_1D.pos_s)))
+            sp.sparse.save_npz(path_I + "/I_matrix", I)
+            np.save(path_I + "/III_ind_array", ind_array)
+        else:
+            print("Began laoding I and III_ind_array from ", path_I)
+            I=sp.sparse.load_npz(path_I + "/I_matrix.npz")
+            self.III_ind_array=np.load(path_I + "/III_ind_array.npy")
+            print("Finished laoding I and III_ind_array from ", path_I)
         self.I_matrix=I
         
         return I
     
-    def AssemblyProblem(self, mat_path,path_phi_bar):
-        Full_linear_matrix=sp.sparse.vstack((self.AssemblyABC(mat_path),
-                                             self.AssemblyDEFFast(path_phi_bar),
-                                             self.AssemblyGHI()))
+    def AssemblyProblem(self, path_matrices):
+        Full_linear_matrix=sp.sparse.vstack((self.AssemblyABC(path_matrices),
+                                             self.AssemblyDEFFast(path_matrices + "/E_portion", path_matrices),
+                                             self.AssemblyGHI(path_matrices)))
         self.Full_linear_matrix=Full_linear_matrix
         
         self.Full_ind_array=np.concatenate((self.I_ind_array, np.zeros(len(self.mesh_1D.pos_s)), self.III_ind_array))

src_final/post_processing.py
@@ -56,11 +56,7 @@ pylab.rcParams.update(params)
 
 import time
 
-#@dask.delayed
-def InterpolateHelper(args):
-    prob,coords=args
-    a,b,c,d = prob.Interpolate(coords)
-    return a.dot(prob.s[b]) + c.dot(prob.q[d]) 
+
 
 class Visualization3D():
     def __init__(self,lim, res, prob, num_proc, vmax, *trans):
@@ -104,7 +100,22 @@ class Visualization3D():
                 data[i*3+j]=b.reshape(res, res)
                 
         self.data=data
-        
+# =============================================================================
+#         arg=0
+#         for i in range(3):
+#             for j in range(3):
+#                 #cor are the corners of a square 
+#                 if i==0: cor=perp_x
+#                 if i==1: cor=perp_y
+#                 if i==2: cor=perp_z
+#                 
+#                 arg.append(self.getCoordReconstChat(cor[j], res, num_processes=num_proc))
+#                 a,b=self.getCoordReconstChat(cor[j], res, num_processes=num_proc)
+#                 
+#                 data[i*3+j]=b.reshape(res, res)
+#                 
+#         self.data=data
+# =============================================================================
         self.perp_x=perp_x
         self.perp_y=perp_y
         self.perp_z=perp_z
@@ -220,22 +231,81 @@ def get_coord_reconst_chat(prob, corners, resolution, num_processes=4):
         
         crds=np.vstack((crds, arr))
     
-    # =============================================================================
-    #         # Create a pool of worker processes
-    #         pool = Pool(processes=num_processes)
-    #         
-    #         # Use map function to apply interpolate_helper to each coordinate in parallel
-    #         results = pool.map(interpolate_helper, [(prob, k) for k in crds])
-    #         
-    #         # Close the pool to free up resources
-    #         pool.close()
-    #         pool.join()
-    #         
-    #         # Convert the results to a numpy array
-    #         rec = np.array(results)
-    # =============================================================================
-    rec=np.array([])        
-    for k in crds:
-        rec=np.append(rec, InterpolateHelper((prob, k)))
+    # Create a pool of worker processes
+    pool = Pool(processes=num_processes)
     
-    return crds, rec     
+    # Use map function to apply interpolate_helper to each coordinate in parallel
+    results = pool.map(InterpolateHelper, [(prob, k) for k in crds])
+    
+    # Close the pool to free up resources
+    pool.close()
+    pool.join()
+    
+    # Convert the results to a numpy array
+    rec = np.array(results)
+# =============================================================================
+#     rec=np.array([])        
+#     for k in crds:
+#         rec=np.append(rec, InterpolateHelper((prob, k)))
+# =============================================================================
+    
+    return crds, rec   
+
+@njit
+def ReconstructionCoordinatesFast(crds, n, cells_x, cells_y,cells_z, h_3D,pos_cells,s_blocks, source_edge,tau, pos_s, h_1D, R, D, s, q):
+    """This function is thought to estimate the concentration field at a given set of coordinates 
+    using parallel dask computation"""
+    print("executing")
+    phi=np.zeros(len(crds), dtype=np.float64)
+    e=0
+    
+    for i in crds:
+        a,b,c,d=InterpolateFast(i, n, cells_x, cells_y,
+                              cells_z, h_3D,GetBoundaryStatus(i,h_3D, cells_x, cells_y, cells_z), 
+                              pos_cells,s_blocks, source_edge,
+                              tau, pos_s, h_1D, R, D)
+        phi[e]=a.dot(s[b]) + c.dot(q[d]) 
+        e+=1
+    return phi
+
+@njit(parallel=True)
+def ReconstructionCoordinatesParallel(crds, n, cells_x, cells_y,cells_z, h_3D,pos_cells,s_blocks, source_edge,tau, pos_s, h_1D, R, D, s, q):
+    """This function is thought to estimate the concentration field at a given set of coordinates 
+    using parallel dask computation"""
+    print("executing")
+    phi=np.zeros(len(crds), dtype=np.float64)
+    e=0
+    
+    for j in prange(len(crds)):
+        i=crds[j]
+        a,b,c,d=InterpolateFast(i, n, cells_x, cells_y,
+                              cells_z, h_3D,GetBoundaryStatus(i,h_3D, cells_x, cells_y, cells_z), 
+                              pos_cells,s_blocks, source_edge,
+                              tau, pos_s, h_1D, R, D)
+        phi[j]=a.dot(s[b]) + c.dot(q[d]) 
+        e+=1
+    return phi
+
+
+# =============================================================================
+# def ReconstructionCoordinatesFast(crds, prob):
+#     """This function is thought to estimate the concentration field at a given set of coordinates 
+#     using parallel dask computation"""
+#     phi=[]
+#     c=0
+#     for i in crds:
+#         phi.append(InterpolateHelperDask((prob, i)))
+#         c+=1
+#     return dask.compute(phi)
+# =============================================================================
+
+def InterpolateHelper(args):
+    prob,coords=args
+    a,b,c,d = prob.Interpolate(coords)
+    return a.dot(prob.s[b]) + c.dot(prob.q[d]) 
+
+@dask.delayed
+def InterpolateHelperDask(args):
+    prob,coords=args
+    a,b,c,d = prob.Interpolate(coords)
+    return a.dot(prob.s[b]) + c.dot(prob.q[d]) 